35e90dd2a60fb8c9a52fe1f23fb06ed49780ec06
protestcorp <85497328+protestcorp@users.noreply.github.com>
2021-10-26 15:07:50 +0530

    Update DataXMLHandler.java

--- src/main/java/CucumberOptionsss/DataXMLHandler.java
+++ src/main/java/CucumberOptionsss/DataXMLHandler.java
@@ -13,11 +13,7 @@ public int getNumberOfTags(String testCaseName, String tagName)
 int length = 0;
 try
 {
-//<<<<<<< HEAD:src/main/java/CucumberOptionsss/DataXMLHandler.java
-//File file = new File("C:\Users\CHETAN S\Downloads\Updated-Demoblaze\Updtd-cucumber-starter\src\main\java\TestData\DATA.xml");
-//=======
-File file = new File("/Demo-cucumber-starter/src/main/java/TestData/DATA.xml");
-//>>>>>>> febe54675e92adf3a4a1de59b3cd7d751f2a9df2:src/test/java/CucumberOptionsss/DataXMLHandler.java
+File file = new File("\\serenity-cucumber-starter-master\\src\\main\\java\\TestData\\DATA.xml");
 DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 Document doc = dBuilder.parse(file);
@@ -47,11 +43,7 @@ String data = "";
 
 try
 {
-//<<<<<<< HEAD:src/main/java/CucumberOptionsss/DataXMLHandler.java
-//File file = new File("C:\Users\CHETAN S\Downloads\Updated-Demoblaze\Updtd-cucumber-starter\src\main\java\TestData\DATA.xml");
-//=======
-File file = new File("/Demo-cucumber-starter/src/main/java/TestData/DATA.xml");
-//>>>>>>> febe54675e92adf3a4a1de59b3cd7d751f2a9df2:src/test/java/CucumberOptionsss/DataXMLHandler.java
+File file = new File("\\serenity-cucumber-starter-master\\src\\main\\java\\TestData\\DATA.xml");
 DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 Document doc = dBuilder.parse(file);
